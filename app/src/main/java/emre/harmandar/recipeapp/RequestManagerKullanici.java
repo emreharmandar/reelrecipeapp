068bf0282d23fff8c6cf4863242d4a2a5c9a2834
Emre Harmandar <emreharmandar1@hotmail.com>
2022-12-19 21:29:53 +0300

    Kullanıcı tariflerine yönlendiren bisiler

---
+++ app/src/main/java/emre/harmandar/recipeapp/RequestManagerKullanici.java
@@ -0,0 +1,89 @@
+package emre.harmandar.recipeapp;
+
+import android.content.Context;
+
+import java.util.List;
+
+import emre.harmandar.recipeapp.Listeners.RandomRecipeResponseListener;
+import emre.harmandar.recipeapp.Listeners.RecipeDetailsListener;
+import emre.harmandar.recipeapp.Models.RandomRecipeApiResponse;
+import emre.harmandar.recipeapp.Models.RecipeDetailsResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public class RequestManagerKullanici {
+    Context context;
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.spoonacular.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+
+    public RequestManagerKullanici(Context context) {
+        this.context = context;
+    }
+
+    public void getRandomRecipes(RandomRecipeResponseListener listener, List<String> tags){
+        CallRandomRecipes callRandomRecipes = retrofit.create(CallRandomRecipes.class);
+        Call<RandomRecipeApiResponse> call = callRandomRecipes.callRandomRecipe(context.getString(R.string.api_key), "10", tags);
+        call.enqueue(new Callback<RandomRecipeApiResponse>() {
+            @Override
+            public void onResponse(Call<RandomRecipeApiResponse> call, Response<RandomRecipeApiResponse> response) {
+                if (!response.isSuccessful()){
+                    listener.didError(response.message());
+                    return;
+                }
+                listener.didFetch(response.body(), response.message());
+            }
+
+            @Override
+            public void onFailure(Call<RandomRecipeApiResponse> call, Throwable t) {
+                listener.didError(t.getMessage());
+
+            }
+        });
+    }
+
+    public void getRecipeDetails(RecipeDetailsListener listener, int id){
+        CallRecipeDetails callRecipeDetails= retrofit.create(CallRecipeDetails.class);
+        Call<RecipeDetailsResponse> call = callRecipeDetails.callRecipeDetails(id, context.getString(R.string.api_key));
+        call.enqueue(new Callback<RecipeDetailsResponse>() {
+            @Override
+            public void onResponse(Call<RecipeDetailsResponse> call, Response<RecipeDetailsResponse> response) {
+                if (!response.isSuccessful()){
+                   listener.didError(response.message());
+                   return;
+                }
+                listener.didFetch(response.body(), response.message());
+            }
+
+            @Override
+            public void onFailure(Call<RecipeDetailsResponse> call, Throwable t) {
+                listener.didError(t.getMessage());
+            }
+        });
+    }
+
+    private interface CallRandomRecipes{
+        @GET("recipes/random")
+        Call<RandomRecipeApiResponse> callRandomRecipe(
+                @Query("apiKey") String apiKey,
+                @Query("number") String number,
+                @Query("tags") List<String> tags
+                );
+    }
+    private interface CallRecipeDetails{
+        @GET("recipes/{id}/information")
+        Call<RecipeDetailsResponse> callRecipeDetails(
+                @Path("id") int id,
+                @Query("apiKey") String apiKey
+        );
+    }
+
+}
